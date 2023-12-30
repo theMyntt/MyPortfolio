94b5926870c7c479b5eea3f2867e828527c0d1e1
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2023-12-29 21:55:32 -0300

    feat: Tela de error 404 (pagina não encontrada)

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import "./index.scss";
+
+function back() {
+  return window.history.back();
+}
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Error 404</h1>
+    <div className="ops">
+      <div>
+        <h1>Ops... não consegui encontrar essa página!</h1>
+        <p>
+          Estou trabalhando para que sua experiencia seja a melhor possível,
+          sinto muito não encontrar o que desejava...
+        </p>
+        <button onClick={back}>Voltar</button>
+      </div>
     </div>
   );
 }
