fd607f4cc1144665eae8792cfc3425984a17b50d
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2023-12-29 22:59:44 -0300

    feat: Alterações para estilização no sass

--- src/Masterpage/index.jsx
+++ src/Masterpage/index.jsx
@@ -5,7 +5,7 @@ export default function MasterPage() {
   return (
     <div className="flex alCenter header">
       <div className="brand-logo">
-        <h2>Gabriel Araújo Lima</h2>
+        <h2 id="logo">Gabriel Araújo Lima</h2>
       </div>
       <div className="brand-nav">
         <nav>
