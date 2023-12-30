6cd8a0bd330a5030cd7c0010c5dc81192509ad92
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2023-12-29 21:54:55 -0300

    feat: menu de navegação

--- src/Masterpage/index.jsx
+++ src/Masterpage/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import "./index.scss";
 
 export default function MasterPage() {
-    return (
-        <div>
-            <h1>Masterpage</h1>
-        </div>
-    );
+  return (
+    <div className="flex alCenter header">
+      <div className="brand-logo">
+        <h2>Gabriel Araújo Lima</h2>
+      </div>
+      <div className="brand-nav">
+        <nav>
+            <ul className="flex">
+                <li><a href="./">Inicio</a></li>
+                <li><a href="./project">Projetos</a></li>
+                <li><a href="./contact">Contato</a></li>
+            </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
