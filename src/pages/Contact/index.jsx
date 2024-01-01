a317cbb2287c424ff988df9ba6cf7b1a5e286fa8
Gabriel Araujo Lima <gabriel.araujo2902@outlook.com>
2024-01-01 12:02:40 -0300

    feat: pagina de contato

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import "./index.scss";
 
+function toWhatsapp() {
+  const nome = document.getElementById("name").value;
+  window.open(`https://api.whatsapp.com/send?phone=5511992046942&text=Olá, eu sou ${nome} e vim pelo seu portfólio.`, "_blank");
+}
+
 export default function Contact() {
   return (
-    <div>
-      <h1>contact</h1>
+    <div className="container">
+      <div className="brand-card">
+        <h2>Que tal falar comigo?</h2>
+        <article>
+          <p>Achou meu perfil interessante? Aqui abaixo deixarei minhas principais formas de contato!</p> <br />
+          <ul>
+            <li>Email: gabriel.araujo2902@outlook.com</li>
+            <li>Telefone e Whatsapp: +55 (11) 99204-6942</li>
+          </ul>
+        </article> <br />
+        <div>
+          <h2>Me mande uma mensagem no WhatsApp</h2>
+          <form onSubmit={toWhatsapp}>
+            <label>Insira seu nome:<input type="text" id="name" placeholder="Digite seu nome aqui!"/></label>
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
