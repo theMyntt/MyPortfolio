738d1d65e6fc2350ba041627ce9d068ea844c054
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-02-05 22:09:59 -0300

    feat: anchor para email e telefone

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -14,8 +14,8 @@ export default function Contact() {
         <article>
           <p>Achou meu perfil interessante? Aqui abaixo deixarei minhas principais formas de contato!</p> <br />
           <ul>
-            <li>Email: <br/> gabriel.araujo2902@outlook.com</li>
-            <li>Telefone: <br /> +55 (11) 99204-6942</li>
+            <li>Email: <br/> <a href="mailto:gabriel.araujo2902@outlook.com">gabriel.araujo2902@outlook.com</a></li>
+            <li>Telefone: <br /> <a href="tel:+5511992046942">+55 (11) 99204-6942</a></li>
           </ul>
         </article> <br />
         <div>
