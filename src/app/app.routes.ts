6e2637931a0cc846dec427511cf189d3abe455ab
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-25 15:48:42 -0300

    feat: experience route page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ExperienceComponent } from './components/experience/experience.component';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'experience',
+    component: ExperienceComponent,
     pathMatch: 'full'
   },
   {
