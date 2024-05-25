0c5524f1e81bd5228345f8017637559539a31acb
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-25 16:32:59 -0300

    feat: back button on experience page

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ButtonComponent],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.sass'
 })
 export class ExperienceComponent {
-
+  goBack() {
+    history.back()
+  }
 }
