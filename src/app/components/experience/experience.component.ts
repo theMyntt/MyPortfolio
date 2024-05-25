12e9ca03b6beb88eea7433003b8ec9757df65ae8
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-25 15:50:54 -0300

    feat: header component at experience component

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-experience',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.sass'
 })
