cbaeb29a82ea3ad16c51e56a0d2cd1cdb33b27b9
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-25 16:42:04 -0300

    feat: header component at contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.sass'
 })
