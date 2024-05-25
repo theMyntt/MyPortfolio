85174e574a268a2efd81c6c55fbfd4992bc3f209
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-25 13:34:03 -0300

    refactor: remove unnecessary html file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,12 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  template: `
+    <div id="wrapper">
+      <router-outlet />
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'MyPortfolio';
+  
 }
