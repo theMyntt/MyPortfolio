2e679c0d7c9f6e71256b4331b75515629d057647
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-25 13:32:03 -0300

    feat: card component

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="card">
+      <div class="card-image">
+        <img src="assets/images/{{image}}" alt="angular">
+      </div>
+      <div class="card-content">
+        <h3>{{title}}</h3>
+        <p>{{description}}</p>
+      </div>
+      <div class="card-action">
+        <a href="{{link}}" target="_blank">Learn More</a>
+      </div>
+    </div>
+  `,
+  styleUrl: './card.component.sass'
+})
+export class CardComponent {
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() image: string = '';
+  @Input() link: string = '';
+}
