012922b6171dbd1ea30850b97a19c9a8a4e02a9e
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-25 12:50:17 -0300

    feat: header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  template: `
+    <div class="header">
+      
+    </div>
+  `,
+  styleUrl: './header.component.sass'
+})
+export class HeaderComponent {
+
+}
