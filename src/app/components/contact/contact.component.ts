a2cbb78e25f40c5f189cfff450a122c664555864
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-25 16:56:50 -0300

    feat: whatsapp send message method

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ButtonComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.sass'
 })
 export class ContactComponent {
+  private name: string = ""
 
+  setName(e: any) {
+    this.name = e.target.value;
+  }
+  
+  goBack() {
+    history.back()
+  }
+
+  sendMessage() {
+    if (!this.name) {
+      alert("Please enter your name");
+      return;
+    }
+    window.open("https://wa.me/5511992046942?text=Hi,%20I'm%20" + encodeURIComponent(this.name), "_blank");
+  }
 }
