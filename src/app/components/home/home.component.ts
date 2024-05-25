e6728d4b71bc4ffb3736d089d5856ba1a960a35b
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-05-25 15:31:13 -0300

    feat: home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
+import { CardComponent } from '../card/card.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, CardComponent, ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass'
 })
 export class HomeComponent {
-
+  redirect(where: string) {
+    location.href = where
+  }
 }
